034ff08b4c634713ca7dcd430ac43662c0706ec6
Atul Singh <73229173+sekharatulsingh@users.noreply.github.com>
2026-09-21 09:10:13 +0530

    Create variables.tf

---
+++ infra/envs/prod/variables.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
