43ec1e11047df19ac365e05b435df76d0c1d55aa
Atul Singh <73229173+sekharatulsingh@users.noreply.github.com>
2026-09-21 09:01:04 +0530

    Create outputs.tf

---
+++ infra/modules/ecs/outputs.tf
@@ -0,0 +1,11 @@
+output "ecs_security_group_id" {
+  value = aws_security_group.ecs.id
+}
+
+output "alb_dns_name" {
+  value = aws_lb.this.dns_name
+}
+
+output "cluster_name" {
+  value = aws_ecs_cluster.this.name
+}
