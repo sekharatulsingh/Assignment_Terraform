36941b7f0962bacc2254888a01747addac34990e
Atul Singh <73229173+sekharatulsingh@users.noreply.github.com>
2026-09-21 09:08:18 +0530

    Create version.tf

---
+++ infra/envs/prod/version.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+}
